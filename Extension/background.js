a356fe178133393e9f27f2db5b64378ee98b727d
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-04-03 16:09:29 +0530

    Two triggers working

--- Extension/background.js
+++ Extension/background.js
@@ -13,62 +13,43 @@
 //this flag request should also send a notification to our mobile email msg or wtsp
 //so as the parent side gets informed when the child is exposed to some explicit content
 
+//this would be called by event listeners which initself calls the capture screenshot functiuon
 
-//gets logged when extension is installed
-chrome.runtime.onInstalled.addListener(function(){
-  chrome.tabs.create({
-    url:'./welcome.html',
-  })
+
+//INACTVITY SOLVED
+
+chrome.idle.setDetectionInterval(16); // Set idle time to 15 seconds
+
+chrome.idle.onStateChanged.addListener((newState) => {
+    if (newState === "idle") {
+        captureScreenshot();
+    }
 });
 
 
-// //this is when the child opens a new tab
-// chrome.tabs.onActivated.addListener(function(activeInfo){
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
 //   captureScreenshot();
 // });
 
-// //this injects a script 
-// //so whn the child scrolls functions gets called
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//   if (changeInfo.status === "complete") {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tabId },
-//       function: function() {
-//         window.addEventListener("scroll", function() {
-//           chrome.runtime.sendMessage({ event: "scroll" });
-//         });
-//       }
-//     });
-//   }
-// });
+chrome.tabs.onCreated.addListener(() => {
+  setTimeout(() => captureScreenshot(), 500); // Delay to avoid dragging issue
+});
 
-// //this receives the message from above and sends it this
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-//   if(request.event === "scroll"){
-//     captureScreenshot();
-//   }
-// });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === "complete") {
+      setTimeout(() => captureScreenshot(), 500);
+  }
+});
 
-// //for inactivity
-// chrome.idle.setDetectionInterval(60);
-// chrome.idle.onStateChanged.addListener(function(state){
-//   if(state === "idle" || state === "locked"){
-//     captureScreenshot();
-//   }
-// });
+
+
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "captureScreenshot") {
       captureScreenshot();
   }
 });
-chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
-  if(request.action === "registerd"){
-    console.log('reg');
-    chrome.storage.local.set({parentEmail:request.parentEmail},()=>{
-    console.log('email recorded');
-    })
-  }
-})
+
 
 async function captureScreenshot() {
   let image = await chrome.tabs.captureVisibleTab(null, { format: "png" });
@@ -113,12 +94,11 @@ async function sendToBackend(imageData) {
   }
 }
 
-// function blobToBase64(blob) {
-//   return new Promise((resolve, reject) => {
-//       const reader = new FileReader();
-//       reader.readAsDataURL(blob);
-//       reader.onloadend = () => resolve(reader.result);
-//       reader.onerror = error => reject(error);
-//   });
-// }
-
+function blobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+  });
+}
